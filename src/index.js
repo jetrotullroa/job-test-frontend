ad6fe7c91429fa1d6845588cc14d12b5a5a1c554
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-09-22 12:49:20 +0800

    setup install materializecss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import 'materialize-css/dist/css/materialize.min.css'
+
+// COMPONENTS
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
